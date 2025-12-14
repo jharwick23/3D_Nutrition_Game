d6a1f758b3b0c25aa71532dbfac9e972bfe75823
mahermoe <159480261+mahermoe@users.noreply.github.com>
2025-12-14 16:41:26 -0500

    Removed debug prints from CollectCoin.cs

--- Assets/Scripts/Collectible Scripts/CollectCoin.cs
+++ Assets/Scripts/Collectible Scripts/CollectCoin.cs
@@ -25,8 +25,6 @@ public class CollectCoin : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        Debug.LogWarning("Player entered coin trigger but is missing PlayerControllerV2 component!");
-        print("Coin Triggered");
         if (other.CompareTag("Player"))
         {
             PlayerControllerV2 playerController = other.GetComponent<PlayerControllerV2>();
