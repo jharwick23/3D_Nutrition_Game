2422efde8b232ede6352431f236d468da581cffb
jharwick23 <jharwick12@gmail.com>
2025-12-15 00:03:47 -0500

    Added SFX manager

---
+++ Assets/Scripts/Audio Scripts/SFXManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class SFXManager: MonoBehaviour
+{
+    public static SFXManager Instance;
+    public AudioSource audioSource;
+    public enum SFXType
+    {
+        KnifeSwing,
+        DamageTaken,
+        Death,
+        Heal
+    }
+
+    [Header("Player")]
+    public AudioClip knifeSwing;
+    public AudioClip damageTaken;
+    public AudioClip death;
+    public AudioClip heal;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        if (!audioSource)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+        if (!audioSource)
+        {
+            Debug.LogError("SFXManager is missing an AudioSource!");
+        }
+    }
+
+    public void Play(SFXType type)
+    {
+        if (!audioSource) return;
+
+        switch (type)
+        {
+            case SFXType.KnifeSwing:
+                audioSource.PlayOneShot(knifeSwing);
+                break;
+            
+            case SFXType.DamageTaken:
+                audioSource.PlayOneShot(damageTaken);
+                break;
+
+            case SFXType.Death:
+                audioSource.PlayOneShot(death, 10f);
+                break;
+
+            case SFXType.Heal:
+                audioSource.PlayOneShot(heal, 15f);
+                break;
+        }
+    }
+}
