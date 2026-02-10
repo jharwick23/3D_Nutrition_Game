361c8f65e5fecefe6f2899ef7b6b5a0eccc2d100
mahermoe <159480261+mahermoe@users.noreply.github.com>
2026-02-09 21:36:57 -0500

    Implemented Bullet Shop

---
+++ Assets/Scripts/UI Scripts/BuyBulletsMenu.cs
@@ -0,0 +1,198 @@
+using UnityEngine;
+using TMPro;
+
+public class BuyBulletsMenu : MonoBehaviour
+{
+    private ProjectileGun _projectileGun;
+    [SerializeField] private GameObject buyBulletPanel;
+    private PlayerControllerV2 _playerController;
+    public TextMeshProUGUI OrangeButtonText;
+    public TextMeshProUGUI TomatoButtonText;
+    public TextMeshProUGUI BananaButtonText;
+    public TextMeshProUGUI LemonButtonText;
+    public TextMeshProUGUI CarrotButtonText;
+
+    private void Start()
+    {
+        _projectileGun ??= FindFirstObjectByType<ProjectileGun>();
+        _playerController ??= FindFirstObjectByType<PlayerControllerV2>();
+        UpdateUI();
+    }
+
+    public void EnableUpgradeMenu()
+    {
+        buyBulletPanel.SetActive(true);
+
+        // Disable Crosshair
+        UIHandler _uiHandler;
+        _uiHandler = FindFirstObjectByType<UIHandler>();
+        if (_uiHandler)
+        {
+            _uiHandler.ToggleCrosshair(false);
+        }
+        else
+        {
+            Debug.Log("UI Handler was not set!");
+        }
+
+        // Disable Inputs
+        InputHandlerV2 inputHandler = FindFirstObjectByType<InputHandlerV2>();
+        if (inputHandler)
+        {
+            inputHandler.DisableInputs();
+        }
+        else
+        {
+            Debug.Log("Inputhandler not found!");
+        }
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void DisableUpgradeMenu()
+    {
+        buyBulletPanel.SetActive(false);
+
+        // Enable Crosshair
+        UIHandler _uiHandler;
+        _uiHandler = FindFirstObjectByType<UIHandler>();
+        if (_uiHandler)
+        {
+            _uiHandler.ToggleCrosshair(true);
+        }
+        else
+        {
+            Debug.Log("UI Handler was not set!");
+        }
+
+        // Enable Inputs
+        InputHandlerV2 inputHandler = FindFirstObjectByType<InputHandlerV2>();
+        if (inputHandler)
+        {
+            inputHandler.EnableInputs();
+        }
+        else
+        {
+            Debug.Log("Inputhandler not found!");
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void UpdateUI()
+    {
+        OrangeButtonText.text = "Owned";
+        if (PlayerPrefs.GetInt("Bullet_Tomato", 0) == 1)
+        {
+            TomatoButtonText.text = "Owned";
+        }
+        else
+        {
+            TomatoButtonText.text = "Buy";
+        }
+
+        if (PlayerPrefs.GetInt("Bullet_Banana", 0) == 1)
+        {
+            BananaButtonText.text = "Owned";
+        }
+        else
+        {
+            BananaButtonText.text = "Buy";
+        }
+
+        if (PlayerPrefs.GetInt("Bullet_Lemon", 0) == 1)
+        {
+            LemonButtonText.text = "Owned";
+        }
+        else
+        {
+            LemonButtonText.text = "Buy";
+        }
+
+        if (PlayerPrefs.GetInt("Bullet_Carrot", 0) == 1)
+        {
+            CarrotButtonText.text = "Owned";
+        }
+        else
+        {
+            CarrotButtonText.text = "Buy";
+        }
+        
+    }
+
+    public void OnBuyBulletTomatoButton()
+    {
+        if (PlayerPrefs.GetInt("Coins", 0) < 300 || PlayerPrefs.GetInt("Bullet_Tomato", 0) == 1)
+        {
+            Debug.Log("Not enough coins to buy Tomato Bullets or you already own this bullet!");
+            return;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Bullet_Tomato", 1);
+            _projectileGun.LoadOwnedBullets();
+            _playerController.AddCoins(-300);
+            UpdateUI();
+        }
+    }
+
+    public void OnBuyBulletBananaButton()
+    {
+        if (PlayerPrefs.GetInt("Coins", 0) < 300 || PlayerPrefs.GetInt("Bullet_Banana", 0) == 1)
+        {
+            Debug.Log("Not enough coins to buy Banana Bullets or you already own this bullet!");
+            return;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Bullet_Banana", 1);
+            _projectileGun.LoadOwnedBullets();
+            _playerController.AddCoins(-300);
+            UpdateUI();
+        }
+    }
+
+    public void OnBuyBulletLemonButton()
+    {
+        if (PlayerPrefs.GetInt("Coins", 0) < 300 || PlayerPrefs.GetInt("Bullet_Lemon", 0) == 1)
+        {
+            Debug.Log("Not enough coins to buy Lemon Bullets or you already own this bullet!");
+            return;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Bullet_Lemon", 1);
+            _projectileGun.LoadOwnedBullets();
+            _playerController.AddCoins(-300);
+            UpdateUI();
+        }
+    }
+
+    public void OnBuyBulletCarrotButton()
+    {
+        if (PlayerPrefs.GetInt("Coins", 0) < 300 || PlayerPrefs.GetInt("Bullet_Carrot", 0) == 1)
+        {
+            Debug.Log("Not enough coins to buy Carrot Bullets or you already own this bullet!");
+            return;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Bullet_Carrot", 1);
+            _projectileGun.LoadOwnedBullets();
+            _playerController.AddCoins(-300);
+            UpdateUI();
+        }
+    }
+
+    public void OnWipeBulletsButton()
+    {
+        PlayerPrefs.DeleteKey("Bullet_Tomato");
+        PlayerPrefs.DeleteKey("Bullet_Banana");
+        PlayerPrefs.DeleteKey("Bullet_Lemon");
+        PlayerPrefs.DeleteKey("Bullet_Carrot");
+        _projectileGun.LoadOwnedBullets();
+        UpdateUI();
+    }
+}
