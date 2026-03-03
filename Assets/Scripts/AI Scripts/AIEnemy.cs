2678cd592221d49a2be9cea1f7e993d561fed98f
Alexiz0 <alexis.chavezguzman7@gmail.com>
2026-03-03 16:04:11 -0500

    More Third Level Work, First Level Clipping Fixes

--- Assets/Scripts/AI Scripts/AIEnemy.cs
+++ Assets/Scripts/AI Scripts/AIEnemy.cs
@@ -9,6 +9,7 @@ public class AIEnemy : MonoBehaviour
     public Slider healthSlider;
     public bool isCube;
     private DropService _dropService;
+    [SerializeField] private bool dropItems = true;
 
     void Awake()
     {
@@ -20,12 +21,20 @@ public class AIEnemy : MonoBehaviour
     {
         if (enemyHealth <= 0)
         {
-            _dropService.DropCoin(new Vector3(transform.position.x, transform.position.y, transform.position.z), 10); // Drop Coin
-            int val = Random.Range(0, 1);
-            if (val == 0)
+            if (dropItems)
             {
-                _dropService.DropHeal(new Vector3(transform.position.x, transform.position.y, transform.position.z), 20);
+                _dropService.DropCoin(new Vector3(transform.position.x, transform.position.y, transform.position.z), 10); // Drop Coin
+            
+                int val = Random.Range(0, 1);
+            
+                if (val == 0)
+            
+                {
+                    _dropService.DropHeal(new Vector3(transform.position.x, transform.position.y, transform.position.z), 20);          
+                }
+
             }
+            
             Destroy(gameObject);
         }
     }
